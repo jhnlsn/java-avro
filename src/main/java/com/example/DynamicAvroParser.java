dd613fe6bae99dc4ab25bbd735e5b2d1b8dfbbce
John <jnelson11@gmail.com>
2024-02-29 21:58:39 -0500

    people

--- src/main/java/com/example/DynamicAvroParser.java
+++ src/main/java/com/example/DynamicAvroParser.java
@@ -17,24 +17,24 @@ import java.io.File;
 public class DynamicAvroParser {
         public static void main(String[] args) throws IOException {
 
-        Schema schemaAccount = new Schema.Parser().parse(new File("src/main/resources/avro/account.avsc"));
+        Schema schemaAccount = new Schema.Parser().parse(new File("src/main/resources/avro/person.avsc"));
 
         // Path to the Avro file
-        String pathAccount = "dynamic_accounts.avro";
+        String pathAccount = "dynamic_persons.avro";
 
         // Write data dynamically to an Avro file
         // Uncomment this out to write data to the Avro file
-        // writeGenericRecords(schemaAccount, pathAccount);
+        writeGenericRecords(schemaAccount, pathAccount);
 
         // // Read data dynamically from an Avro file
         // readGenericRecords(schema, path);
 
-        readSpecificRecords(schemaAccount, pathAccount);
+        // readSpecificRecords(schemaAccount, pathAccount);
     }
 
     public static void writeGenericRecords(Schema schema, String path) throws IOException {
         GenericRecord account = new GenericData.Record(schema);
-        account.put("bankIdentifier", "1234");
+        account.put("firstName", "John");
 
         File file = new File(path);
         DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(schema);
@@ -63,11 +63,11 @@ public class DynamicAvroParser {
             File file = new File(path);
 
             // DatumReader is used to de-serialize Avro data from disk
-            SpecificDatumReader<AccountCreated> datumReader = new SpecificDatumReader<>(AccountCreated.class);
+            SpecificDatumReader<PersonCreated> datumReader = new SpecificDatumReader<>(PersonCreated.class);
 
             // Construct a DataFileReader instance to read the data
-            try (DataFileReader<AccountCreated> dataFileReader = new DataFileReader<>(file, datumReader)) {
-                AccountCreated account = null; // Reusable Account object
+            try (DataFileReader<PersonCreated> dataFileReader = new DataFileReader<>(file, datumReader)) {
+                PersonCreated account = null; // Reusable Account object
 
                 // Iterate over the file and print each Account object
                 while (dataFileReader.hasNext()) {
